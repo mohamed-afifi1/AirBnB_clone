5efb5225d066c2a2ae19ded03d147ddbca18a509
root <root@DESKTOP-HU468P5.localdomain>
2024-05-19 02:26:07 +0300

    base

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.name = None
+            self.my_number = None
         else:
             for k in kwargs.keys():
                 if k != "__class__":
