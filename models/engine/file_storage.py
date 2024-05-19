08dcabbc24ca4ecb5721302dd3d3518323ec477c
root <root@DESKTOP-HU468P5.localdomain>
2024-05-19 05:44:14 +0300

    base

---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return all objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Create a new object"""
+        key = type(obj).__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """save into JSON representation"""
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            dictionary = {}
+            for k, v in self.__objects.items():
+                dictionary[k] = v.to_dict()
+            json.dump(dictionary, file)
+
+    def reload(self):
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
+            objects_dict = json.load(file)
+            for k, v in objects_dict.items():
+                objects_dict[k] = self.classes()[v["__class__"]](**v)
+            FileStorage.__objects = objects_dict
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
