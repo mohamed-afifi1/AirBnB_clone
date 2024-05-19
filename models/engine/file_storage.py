25dde4dafe22ecc3534eaeb014465708cc3a0101
root <root@DESKTOP-HU468P5.localdomain>
2024-05-19 06:08:04 +0300

    json

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,44 +38,6 @@ class FileStorage:
                 objects_dict[k] = self.classes()[v["__class__"]](**v)
             FileStorage.__objects = objects_dict
 
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
-
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
